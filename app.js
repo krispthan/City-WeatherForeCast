3ed13cf065fd1aebffc2220bb0e30aa219557184
krispthan <44071872+krispthan@users.noreply.github.com>
2019-05-23 12:38:15 -0700

    display data

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
+"use strict";
 
 searchBtn.addEventListener('click', searchWeather);
 
-function searchWeather(){
+function searchWeather() {
     loadingText.style.display = 'block';
     weatherBox.style.display = 'none';
     let cityName = searchCity.value;
-    if (cityName.length === 0){
-        return alert("Please enter a city name");
+    if (cityName.trim().length == 0) {
+        return alert('Please enter a City Name');
     }
     let http = new XMLHttpRequest();
     let apiKey = 'cfef0f6285eef65c0a99672e3d0b047b';
-    let url =`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`;
-    let method = 'GET'
+    let url = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=metric&appid=' + apiKey;
+    let method = 'GET';
 
-    http.open(method,url);
-    http.onreadystatechange = () => {
-        if(http.readyState === XMLHttpRequest.DONE && http.status === 200){
+    http.open(method, url);
+    http.onreadystatechange = function() {
+        if (http.readyState == XMLHttpRequest.DONE && http.status === 200) {
             let data = JSON.parse(http.responseText);
             let weatherData = new Weather(cityName, data.weather[0].description.toUpperCase());
             weatherData.temperature = data.main.temp;
-           
-        }else if(http.readyState === XMLHttpRequest.DONE){
+            updateWeather(weatherData);
+        } else if (http.readyState === XMLHttpRequest.DONE) {
             alert('Something went wrong!');
         }
     };
@@ -31,6 +32,7 @@ function updateWeather(weatherData) {
     weatherCity.textContent = weatherData.cityName;
     weatherDescription.textContent = weatherData.description;
     weatherTemperature.textContent = weatherData.temperature;
-    loadingText.style.display = 'hidden';
+
+    loadingText.style.display = 'none';
     weatherBox.style.display = 'block';
 }
